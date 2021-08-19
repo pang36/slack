9a0360cafde17fc41a051416c744659f8b7182d7
pang36 <pang36@protonmail.com>
2021-08-19 14:30:00 +0700

    try

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import './App.css';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-    <h1>Test</h1>
+      <Router>
+        <div>
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/users">
+              <Users />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA_OCzQBgCuHAWeSd9kTAXguYTFYfvm-4k",
+  authDomain: "slack-36.firebaseapp.com",
+  projectId: "slack-36",
+  storageBucket: "slack-36.appspot.com",
+  messagingSenderId: "1058283665191",
+  appId: "1:1058283665191:web:77640e1d698f2d6aec9b02",
+  measurementId: "G-LT0XS5W51D"
+};
